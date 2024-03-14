bae96c86fbdbe9b1fa749a5dcc99bb25ae6453c4
AdrianCotrim <adriancotrimdosreis4@gmail.com>
2024-03-14 20:38:45 -0300

    Card Colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = (props) => {
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/AdrianCotrim.png' alt='Adrian Cotrim'></img>
+            </div>
+            <div className='rodape'>
+                <h4>Adrian</h4>
+                <h5>Dev Front-End</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
