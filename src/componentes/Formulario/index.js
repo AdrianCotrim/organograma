f3cbe30a3169812f8a9b66dc73c33b676b64870f
AdrianCotrim <adriancotrimdosreis4@gmail.com>
2024-03-15 20:20:41 -0300

    Escondendo times vazios

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -11,19 +11,23 @@ const Formulario = (props) =>{
     const [imagem, setImagem] = useState("")
     const [time, setTime] = useState("")
 
-    const AoSalvar = (event) => {
-        event.preventDefault()
-        console.log(props.aoColaboradorCadastrado({
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoColaboradorCadastrado({
             nome,
             cargo,
             imagem,
             time
-        }))
+        })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return(
         <section className='formulario'>
-            <form onSubmit={AoSalvar}>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
                 <CampoTexto aoAlterado={valor => setNome(valor)} obrigatorio={true} label='Nome' placeholder='Digite seu nome'></CampoTexto>
                 <CampoTexto aoAlterado={valor => setCargo(valor)} obrigatorio={true} label='Cargo' placeholder='Digite seu cargo'></CampoTexto>
